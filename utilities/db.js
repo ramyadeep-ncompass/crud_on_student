dc9471c3b001fbc5710897dc5b375d07d19a4524
unknown <ramyadeep@ncompass.in>
2022-05-20 11:35:31 +0530

    secured credentials with dotenv

--- utilities/db.js
+++ utilities/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 // get the client
 const mysql = require('mysql2');
 
 // create the connection to database
 module.exports.mySqlConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'usecase2'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
