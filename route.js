a11c180455a3baedbed38177250539f3173c3ad2
unknown <ramyadeep@ncompass.in>
2022-05-20 12:07:26 +0530

    changed hhtp methods where necessary

--- route.js
+++ route.js
@@ -8,14 +8,14 @@ const {
     createStudent
 } = require('./controllers/student-controllers');
 
-router.get('/create/student/new', createStudent);
+router.post('/create/student/new', createStudent);
 
 router.get('/read/student/all', getAllStudent);
 
 router.get('/read/student/', getStudent);
 
-router.get('/update/student', updateStudent);
+router.patch('/update/student', updateStudent);
 
-router.get('/delete/student', deleteStudent);
+router.delete('/delete/student', deleteStudent);
 
 module.exports = { router };
