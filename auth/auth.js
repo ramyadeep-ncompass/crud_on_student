55c82532a698757ec91aa160985c2a2c268d0432
unknown <ramyadeep@ncompass.in>
2022-05-22 21:48:32 +0530

    implemented basic bearer token authentication

--- auth/auth.js
+++ auth/auth.js
@@ -1,5 +1,27 @@
+const config = require('config');
+
 module.exports.authenticateStudent = (req, res, next) => {
+
+    const auth = config.get('authentication');
     console.log('This is a middleware');
-    console.log((req.headers));
+
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (token !== auth.api_token) {
+            res.status(401).send({
+                success: false,
+                message: "You are not authorized",
+                data: {}
+            });
+            return
+        }
+    } catch {
+        res.status(400).send({
+            success: false,
+            message: "Token not found",
+            data: {}
+        });
+        return
+    }
     next();
 }
